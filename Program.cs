ca1df2a749aa4ae40818d950b8e7065a9606d3dc
Bill Chin <wchin@mdimension.com>
2024-01-24 17:24:17 -0500

    Initial version

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inputTemperature = 35.8;
+
+            Console.WriteLine("Input Temperature is " + inputTemperature);
+        }
+    }
+}
