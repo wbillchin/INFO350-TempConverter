c00ebe0690ff55ec403586ea0a784d4a8db9f2bf
Bill Chin <chinww@vcu.edu>
2024-09-04 21:35:01 -0400

    More updates

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
+/*
+ * INFO 350 Temperature Conversion Assignment
+ * 
+ * Modify this template as needed as directed by the assignment,
+ * including the output message(s).
+ * 
+ */
+
+
 using System;
 
 namespace MyApp // Note: actual namespace depends on the project name.
@@ -7,9 +16,9 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             double inputTemperature = 35.8;
-            double aNewNumber = 3.2;
 
             Console.WriteLine("Input Temperature is " + inputTemperature);
+
         }
     }
 }
